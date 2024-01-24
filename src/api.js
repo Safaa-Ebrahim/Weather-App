a32016cc8a561f629dbe0cb6aa825a4d8c3dfb15
Safaa-Ebrahim <ebrasafa@gmail.com>
2024-01-24 23:06:14 +0200

    add api url and key

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: 'GET',
+	headers: {
+        'X-RapidAPI-Key': '28a4a3bc14msh5f659878e696dc9p1b7d2ejsn49afb62d8733',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_APT_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL="https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY="6b945ee7f96a34c9663ad477e610620d";
