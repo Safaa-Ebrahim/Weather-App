96c83495c01dbd11006a4488d0c08e270a3c5638
Safaa-Ebrahim <ebrasafa@gmail.com>
2024-01-24 23:05:28 +0200

    add search drobmenu and fetch data

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_APT_URL, geoApiOptions } from "../../api";
 
-const Search = () => {
+const Search = ({ onSearchChange }) => {
+  const [search, setSearch] = useState(null);
+
+  const handelOnChange = (searchData) => {
+    setSearch(searchData);
+    // onSearchChange(searchData);
+  };
+
+  const loadOption = async (value) => {
+    try {
+      const response = await fetch(
+        `${GEO_APT_URL}/cities?minPopulation=1000000&namePrefix=${value}`,
+        geoApiOptions
+      );
+      const result = await response.json();
+      return {
+        options: result.data.map((city) => {
+          console.log(city);
+          return {
+            value: `${city.latitude} ${city.longitude}`,
+            label: `${city.name} ${city.countryCode}`,
+          };
+        }),
+      };
+    } catch (err) {
+      return console.log(err);
+    }
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <AsyncPaginate
+      placeholder="Search For City"
+      debounceTimeout={600}
+      value={search}
+      onChange={handelOnChange}
+      loadOptions={loadOption}
+    />
+  );
+};
 
-export default Search
+export default Search;
