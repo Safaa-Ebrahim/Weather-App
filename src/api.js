d2af491400564e782b86025e30776043d92871e9
Safaa-Ebrahim <ebrasafa@gmail.com>
2024-01-26 15:48:38 +0200

    add weather function and geoapi function

--- src/api.js
+++ src/api.js
@@ -1,11 +1,43 @@
-export const geoApiOptions = {
-    method: 'GET',
-	headers: {
-        'X-RapidAPI-Key': '28a4a3bc14msh5f659878e696dc9p1b7d2ejsn49afb62d8733',
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-	}
-};
-export const GEO_APT_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
-export const WEATHER_API_URL="https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY="6b945ee7f96a34c9663ad477e610620d";
+const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "28a4a3bc14msh5f659878e696dc9p1b7d2ejsn49afb62d8733",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export async function fetchCities(input) {
+  try {
+    const response = await fetch(
+      `${GEO_API_URL}/cities?minPopulation=10000&namePrefix=${input}`,
+      geoApiOptions
+    );
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+}
+export async function fetchWeatherData(lat, lon) {
+  try {
+    let [weatherPromise, forcastPromise] = await Promise.all([
+      fetch(
+        `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+      ),
+      fetch(
+        `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+      ),
+    ]);
+
+    const weatherResponse = await weatherPromise.json();
+    const forcastResponse = await forcastPromise.json();
+    return [weatherResponse, forcastResponse];
+  } catch (error) {
+    console.log(error);
+  }
+}
